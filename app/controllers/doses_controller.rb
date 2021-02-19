7b5733e24e08b0d707082651a13c1fe8aba572e9
Amara Cole <amara.cole@yahoo.com>
2021-02-19 11:54:23 +0100

    removed gemfile lock

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,7 +16,9 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @dose
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
